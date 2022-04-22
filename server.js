64ff2919f2e4325142ce981f85b55e5bdc590308
K1neticz <ejoliver@ymail.com>
2022-04-22 15:40:34 -0700

    User can now login

--- server.js
+++ server.js
@@ -3,10 +3,14 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session');
+var passport = require('passport');
 var methodOverride = require('method-override');
 
 require('dotenv').config(); 
 require('./config/database');
+// configure passport
+require('./config/passport');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -22,9 +26,33 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(methodOverride('_method'));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+const isLoggedIn = require('./config/auth');
+
+// Session middleware
+app.use(session({
+  secret: process.env.SECRET,
+  resave: false,
+  saveUninitialized: true
+}));
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Make user available within every EJS template
+app.use(function(req, res, next) {
+  res.locals.user = req.user;
+  next();
+});
+
+// Make user available within every EJS template
+app.use(function(req, res, next) {
+  res.locals.user = req.user;
+  next();
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
