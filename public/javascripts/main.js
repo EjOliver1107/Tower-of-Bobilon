e23763b5a39a622e9e4bb48028b0fd4af6e8d3a3
K1neticz <ejoliver@ymail.com>
2022-04-28 23:41:57 -0700

    Extra CSS in list page

---
+++ public/javascripts/main.js
@@ -0,0 +1,24 @@
+/*----- constants -----*/
+
+
+
+/*----- app's state (variables) -----*/
+
+
+
+/*----- cached element references -----*/
+
+
+
+/*----- event listeners -----*/
+
+
+
+/*----- functions -----*/
+
+function optSwitch() {
+
+}
+// if a user selects a opt from the img select tag then
+// it will reperesnt itself as an image.
+console.log('Hello World!')
